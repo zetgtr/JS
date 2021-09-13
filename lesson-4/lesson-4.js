084a62995fb950f2bc7dabda50046e7286e1fbd5
zetgtr <artem.kudrin70@gmail.com>
2021-09-13 15:00:30 +0300

    выполнил дз-4+игра

---
+++ lesson-4/lesson-4.js
@@ -0,0 +1,18 @@
+var obj = {}
+n = prompt("Введите чсило от 0 - 999")
+var num = Number(n)
+    if (num < 10 && num >= 0){
+        obj.ed = n[0]
+    }
+    if (num >= 10 && num < 100){
+        obj.des = n[0]
+        obj.ed = n[1]
+    }
+    if (num >= 100 && num < 1000){
+        obj.hunders = n[0]
+        obj.des = n[1]
+        obj.ed = n[2]
+    }else {
+        console.log("Ошибка! Вы ввели недопустимое кол-во символов")
+    }
+console.log(obj) // я решил через if сделать
